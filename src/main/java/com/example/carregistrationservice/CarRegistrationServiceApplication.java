6f2fd6325ee216416b7b7a3a47d615916251870b
Marcos Vinicio <129138202+marcosviniciosampaio@users.noreply.github.com>
2023-07-12 16:50:07 -0300

    initial-commit

---
+++ src/main/java/com/example/carregistrationservice/CarRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.carregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carregistrationservice/CarRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
