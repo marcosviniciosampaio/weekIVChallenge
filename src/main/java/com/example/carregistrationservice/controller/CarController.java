32ef68b88c1c74566ae2121f7493390919a22bfc
Marcos Vinicio <129138202+marcosviniciosampaio@users.noreply.github.com>
2023-07-16 02:24:18 -0300

    Invalid brands exception treatment

--- src/main/java/com/example/carregistrationservice/controller/CarController.java
+++ src/main/java/com/example/carregistrationservice/controller/CarController.java
@@ -3,9 +3,11 @@ package com.example.carregistrationservice.controller;
 import com.example.carregistrationservice.dto.CarDtoRequest;
 import com.example.carregistrationservice.dto.CarDtoResponse;
 import com.example.carregistrationservice.entity.Car;
+import com.example.carregistrationservice.exception.ApiRequestException;
 import com.example.carregistrationservice.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(value = "/car")
@@ -16,11 +18,19 @@ public class CarController {
 
     @PostMapping("/post")
     public Car post(@RequestBody CarDtoRequest carDtoRequest){
-        return carService.save(carDtoRequest);
+        if (carDtoRequest.getBrand().equalsIgnoreCase(("ford")) ||
+                (carDtoRequest.getBrand().equalsIgnoreCase(("bmw"))) ||
+                (carDtoRequest.getBrand().equalsIgnoreCase(("chevrolet"))) ||
+                (carDtoRequest.getBrand().equalsIgnoreCase(("volvo")))) {
+            return carService.save(carDtoRequest);
+        }else{
+            throw new ApiRequestException(" Invalid Brand. ");
+        }
     }
+
 
     @GetMapping ("/get/{id}")
     public CarDtoResponse get(@PathVariable Long id) {
-      return carService.getCarById(id);
+        return carService.getCarById(id);
     }
 }
--- src/main/java/com/example/carregistrationservice/service/CarService.java
+++ src/main/java/com/example/carregistrationservice/service/CarService.java
@@ -3,15 +3,13 @@ package com.example.carregistrationservice.service;
 import com.example.carregistrationservice.dto.CarDtoRequest;
 import com.example.carregistrationservice.dto.CarDtoResponse;
 import com.example.carregistrationservice.entity.Car;
-import com.example.carregistrationservice.exception.BrandNotFoundException;
-import com.example.carregistrationservice.exception.CarNotFoundException;
+import com.example.carregistrationservice.exception.ApiRequestException;
 import com.example.carregistrationservice.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
-public class CarService {
 
+public class CarService {
 
     @Autowired
     CarRepository carRepository;
@@ -19,27 +17,21 @@ public class CarService {
     Car car;
 
     public Car save(CarDtoRequest carDtoRequest) {
-        Car car = new Car(
+          Car car = new Car(
                 null,
                 carDtoRequest.getName(),
                 carDtoRequest.getBrand(),
                 carDtoRequest.getColor(),
                 carDtoRequest.getFabricationYear()
         );
-
-        if (carDtoRequest.getBrand().equalsIgnoreCase(("ford")) ||
-                (carDtoRequest.getBrand().equalsIgnoreCase(("bmw"))) ||
-                (carDtoRequest.getBrand().equalsIgnoreCase(("chevrolet"))) ||
-                (carDtoRequest.getBrand().equalsIgnoreCase(("volvo")))) {
-            carRepository.save(car);
-        }
-        return car;
+          carRepository.save(car);
+          return car;
     }
 
     public CarDtoResponse getCarById(Long idChassi) {
         Car car =  carRepository
                 .findById(idChassi)
-                .orElseThrow(() -> new RuntimeException("Car could not be found"));
+                .orElseThrow(() -> new ApiRequestException("Car could not be found"));
 
         CarDtoResponse carDtoResponse = new CarDtoResponse(
                 car.getIdChassi(),
